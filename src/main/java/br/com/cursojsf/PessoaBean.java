bf109a1b27b1d1c448207867c42c602a28fa885f
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2024-10-17 11:26:48 -0300

    Projeto criado

---
+++ src/main/java/br/com/cursojsf/PessoaBean.java
@@ -0,0 +1,35 @@
+package br.com.cursojsf;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name = "pessoaBean")
+public class PessoaBean {
+
+	private String nome;
+	private String sobrenome;
+	private String nomeCompleto;
+	
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	public void mostarNome() {
+		
+		this.nomeCompleto = this.nome +" "+ this.sobrenome;
+	}
+}
